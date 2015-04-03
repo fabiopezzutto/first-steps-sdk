14c9aad7c8358f9c0b2cf664525afd5ba9493fa1
davidedemartini <davide.demartini@gmail.com>
2015-04-03 17:24:22 +0200

    fisrt portlet commit

---
+++ portlets/first-faq-portlet/docroot/WEB-INF/src/it/smc/portlet/FaqPortlet.java
@@ -0,0 +1,11 @@
+package it.smc.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class FaqPortlet
+ */
+public class FaqPortlet extends MVCPortlet {
+ 
+
+}
